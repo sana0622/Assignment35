f1e27c55a461469420a836e84b5ed5e8df1dfc34
saniya <fsaniya948@gmail.com>
2023-08-23 16:18:28 +0530

    program added with two more features

--- division program.py
+++ division program.py
@@ -1,8 +1,14 @@
 def display(msg):
     print(msg)
 
-def div_num(a,b):
-    sum = a/b
-    display(sum)
-print(div_num(4,5))
+def add_num(a,b):
+    sum = a+b
+    display("the sum is "+str(sum))
+def diff_num(a,b):
+    diff = a-b
+    display("the difference is "+ str(diff))
+
+
 display("this is my first program") 
+add_num(10,12)
+diff_num(34,11)
